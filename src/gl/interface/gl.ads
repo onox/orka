f0c11cf37831ea303b9e389967f65626ef70c526
onox <denkpadje@gmail.com>
2018-02-06 22:46:43 +0100

    gl: Remove unused Not_Implemented_Exception exception
    Signed-off-by: onox <denkpadje@gmail.com>

--- src/gl/interface/gl.ads
+++ src/gl/interface/gl.ads
@@ -16,36 +16,32 @@ with Interfaces.C;
 
 package GL is
    pragma Preelaborate;
-   
+
    package C renames Interfaces.C;
-   
+
    -----------------------------------------------------------------------------
    --                                 Basics                                  --
    -----------------------------------------------------------------------------
-   
+
    procedure Flush;
    procedure Finish;
-   
-   -- index types for vectors and matrices
+
+   --  Index types for vectors and matrices
    type Index_Homogeneous is (X, Y, Z, W);
    subtype Index_3D is Index_Homogeneous range X .. Z;
    subtype Index_2D is Index_Homogeneous range X .. Y;
-   
-   -- raised when a function that is not available for the current context
-   -- is called.
+
    Feature_Not_Supported_Exception : exception;
-   
-   -- raised when OpenGLAda does not support a certain OpenGL feature
-   -- (either because it's too new and has not yet been wrapped, or because
-   -- it's so deprecated that you shouldn't use it anyway)
-   Not_Implemented_Exception : exception;
+   --  Raised when a function that is not available for the current
+   --  context is called
+
 private
 
    -----------------------------------------------------------------------------
    --                           Internal functions                            --
    -----------------------------------------------------------------------------
 
-   procedure Raise_Exception_On_OpenGL_Error;
-   pragma Inline (Raise_Exception_On_OpenGL_Error);
+   procedure Raise_Exception_On_OpenGL_Error
+     with Inline;
 
 end GL;
