34896ecbeab0f8df69a2a243a3f87d46d36782fc
onox <denkpadje@gmail.com>
2018-11-12 18:39:07 +0100

    orka: Make package Orka.Terminals private
    Signed-off-by: onox <denkpadje@gmail.com>

--- src/orka/interface/orka-terminals.ads
+++ src/orka/interface/orka-terminals.ads
@@ -12,7 +12,7 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 
-package Orka.Terminals is
+private package Orka.Terminals is
    pragma Preelaborate;
 
    type Style is (Default, Bold, Dark, Italic, Underline, Blink, Reversed, Cross_Out);
