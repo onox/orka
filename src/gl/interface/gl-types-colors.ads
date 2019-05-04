aa103e2a28a3d54b4b8d3a075aa0197b52a2f0c0
onox <denkpadje@gmail.com>
2019-05-05 00:17:25 +0200

    gl: Remove unused type Basic_Color
    Signed-off-by: onox <denkpadje@gmail.com>

--- src/gl/interface/gl-types-colors.ads
+++ src/gl/interface/gl-types-colors.ads
@@ -18,25 +18,19 @@ package GL.Types.Colors is
    pragma Preelaborate;
 
    type Color_Index is (R, G, B, A);
-   subtype Basic_Color_Index is Color_Index range R .. B;
 
    subtype Component is Single range 0.0 .. 1.0;
 
    type Color is array (Color_Index) of aliased Component;
-   type Basic_Color is array (Basic_Color_Index) of Component;
 
    pragma Convention (C, Color);
-   pragma Convention (C, Basic_Color);
 
    type Enabled_Color is array (Color_Index) of Boolean
      with Convention => C;
 
    type Color_Array is array (Size range <>) of aliased Color;
-   type Basic_Color_Array is array (Size range <>) of aliased Basic_Color;
 
    package Color_Pointers is new Interfaces.C.Pointers
      (Size, Color, Color_Array, Color'(others => 0.0));
-   package Basic_Color_Pointers is new Interfaces.C.Pointers
-     (Size, Basic_Color, Basic_Color_Array, Basic_Color'(others => 0.0));
 
 end GL.Types.Colors;
