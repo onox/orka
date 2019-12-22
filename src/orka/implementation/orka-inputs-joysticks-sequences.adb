8f247932ada47c8966224bfd19d7d4813a64b053
onox <denkpadje@gmail.com>
2019-12-22 17:16:17 +0100

    orka: Add package Inputs.Joysticks.Sequences for button sequences
    Signed-off-by: onox <denkpadje@gmail.com>

---
+++ src/orka/implementation/orka-inputs-joysticks-sequences.adb
@@ -0,0 +1,78 @@
+--  SPDX-License-Identifier: Apache-2.0
+--
+--  Copyright (c) 2019 onox <denkpadje@gmail.com>
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+
+package body Orka.Inputs.Joysticks.Sequences is
+
+   use Ada.Real_Time;
+
+   function Create_Sequence
+     (Buttons  : Button_Index_Array;
+      Max_Time : Duration) return Sequence
+   is
+      DT : constant Time_Span := To_Time_Span (Max_Time);
+   begin
+      return
+        (Button_Count => Buttons'Length,
+         Buttons      => Buttons,
+         Index        => Buttons'First,
+         Max_Time     => DT,
+         Start_Press  => Clock - DT);
+   end Create_Sequence;
+
+   function Detect_Activation
+     (Object   : in out Sequence;
+      Joystick : Joystick_Input'Class) return Boolean
+   is
+      Current_Time : constant Time := Clock;
+
+      On_Time : constant Boolean := Current_Time - Object.Start_Press < Object.Max_Time;
+
+      Pressed_Buttons : constant Boolean_Button_States := Joystick.Just_Pressed;
+      Expected_Button : Boolean_Button_States := (others => False);
+   begin
+      Expected_Button (Object.Buttons (Object.Index)) := True;
+      declare
+         Unexpected_Buttons : constant Boolean_Button_States :=
+           Pressed_Buttons and not Expected_Button;
+      begin
+         --  If the user presses a wrong button, it might actually be
+         --  the correct button of the start of the sequence. Therefore,
+         --  do not stop, but just reset the sequence
+         if (for some Button of Unexpected_Buttons => Button) then
+            Object.Index := 1;
+         end if;
+
+         if Pressed_Buttons (Object.Buttons (Object.Index)) then
+            if Object.Index = 1 then
+               Object.Start_Press := Current_Time;
+               Object.Index := Object.Index + 1;
+            elsif On_Time then
+               if Object.Index = Object.Button_Count then
+                  Object.Index := 1;
+                  return True;
+               else
+                  Object.Index := Object.Index + 1;
+               end if;
+            else
+               Object.Index := 1;
+            end if;
+         end if;
+      end;
+
+      return False;
+   end Detect_Activation;
+
+end Orka.Inputs.Joysticks.Sequences;
---
+++ src/orka/interface/orka-inputs-joysticks-sequences.ads
@@ -0,0 +1,49 @@
+--  SPDX-License-Identifier: Apache-2.0
+--
+--  Copyright (c) 2019 onox <denkpadje@gmail.com>
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+
+private with Ada.Real_Time;
+
+package Orka.Inputs.Joysticks.Sequences is
+
+   subtype Sequence_Button_Index is Positive range 1 .. 16;
+
+   type Button_Index_Array is array (Sequence_Button_Index range <>) of Button_Index;
+
+   type Sequence (<>) is tagged private;
+
+   function Create_Sequence
+     (Buttons  : Button_Index_Array;
+      Max_Time : Duration) return Sequence
+   with Pre => Buttons'Length >= 2;
+   --  Return a sequence for the given button indices, which must be
+   --  pressed one after another within a certain duration
+
+   function Detect_Activation
+     (Object   : in out Sequence;
+      Joystick : Joystick_Input'Class) return Boolean;
+   --  Return True if the buttons in the sequence have been pressed,
+   --  False otherwise
+
+private
+
+   type Sequence (Button_Count : Positive) is tagged record
+      Buttons     : Button_Index_Array (1 .. Button_Count);
+      Index       : Sequence_Button_Index;
+      Max_Time    : Ada.Real_Time.Time_Span;
+      Start_Press : Ada.Real_Time.Time;
+   end record;
+
+end Orka.Inputs.Joysticks.Sequences;
