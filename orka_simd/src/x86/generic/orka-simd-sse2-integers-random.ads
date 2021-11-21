f95f78a3c25338b9733ac5fbad6d004e9585bec2
onox <denkpadje@gmail.com>
2021-11-21 16:07:54 +0100

    simd: Add the xoshiro128++ random number generator using SSE2 intrinsics
    A port of the original C implementation [1] to Ada. SIMD intrinsics are
    used to generate multiple numbers at once, instead of one number at a
    time in the C implementation.
    
    [1] "Scrambled linear pseudorandom number generators", Blackman D., Vigna S.,
        ACM Trans. Math. Softw., 47:1−32, 2021
    
    Signed-off-by: onox <denkpadje@gmail.com>

---
+++ orka_simd/src/x86/generic/orka-simd-sse2-integers-random.ads
@@ -0,0 +1,51 @@
+--  SPDX-License-Identifier: Apache-2.0
+--
+--  Copyright (c) 2021 onox <denkpadje@gmail.com>
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+
+with Orka.SIMD.SSE.Singles;
+
+package Orka.SIMD.SSE2.Integers.Random
+  with SPARK_Mode => On
+is
+   pragma Pure;
+
+   use Orka.SIMD.SSE.Singles;
+
+   type State is limited private;
+
+   procedure Next (S : in out State; Value : out m128i)
+     with Inline_Always,
+          Global  => null,
+          Depends => ((S, Value) => S);
+   --  Use and modify the given state to generate multiple
+   --  random unsigned integers
+
+   procedure Next (S : in out State; Value : out m128)
+     with Inline_Always,
+          Global  => null,
+          Depends => ((S, Value) => S);
+   --  Use and modify the given state to generate multiple random
+   --  floating-point numbers in the interval [0, 1).
+
+   procedure Reset (S : out State; Seed : Integer_32)
+     with Global  => null,
+          Depends => (S => Seed),
+          Pre     => Seed /= 0;
+
+private
+
+   type State is array (Integer_32 range 0 .. 3) of m128i;
+
+end Orka.SIMD.SSE2.Integers.Random;
