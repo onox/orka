eb70684aff34b504b4dad9bf41fa80c9929b4e68
onox <denkpadje@gmail.com>
2022-11-09 08:06:08 +0100

    awt: Encode some strings containing UTF-8
    Signed-off-by: onox <denkpadje@gmail.com>

--- awt/src/shared/awt-monitors-log_information.adb
+++ awt/src/shared/awt-monitors-log_information.adb
@@ -1,4 +1,5 @@
 with Orka.Logging.Default;
+with Orka.Strings;
 
 separate (AWT.Monitors)
 procedure Log_Information (Monitor : AWT.Monitors.Monitor'Class) is
@@ -15,6 +16,6 @@ begin
      " monitor " & (+State.Name) & " (" & Trim (Monitor.ID'Image) & ")");
    Log (Info, "  offset:  " & Trim (State.X'Image) & ", " & Trim (State.Y'Image));
    Log (Info, "  size:    " &
-     Trim (State.Width'Image) & " × " & Trim (State.Height'Image));
+     Trim (State.Width'Image) & Orka.Strings.Unicode (" × ") & Trim (State.Height'Image));
    Log (Info, "  refresh: " & Trim (Image (State.Refresh)));
 end Log_Information;
