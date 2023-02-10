713adcba6dbd1b8ee61dd503cfe00f4c08df25f0
onox <denkpadje@gmail.com>
2023-02-10 15:29:20 +0100

    orka: Add package Orka.Rendering.Samplers
    The package contains the type Sampler_State which should contain all the
    state needed for a sampler.
    
    It is currently not used by any code. It should probably be used by the
    render passes of a frame graph.
    
    Signed-off-by: onox <denkpadje@gmail.com>

---
+++ orka/src/orka/interface/orka-rendering-samplers.ads
@@ -0,0 +1,49 @@
+--  SPDX-License-Identifier: Apache-2.0
+--
+--  Copyright (c) 2023 onox <denkpadje@gmail.com>
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+
+with GL.Objects.Samplers;
+with GL.Types.Colors;
+
+package Orka.Rendering.Samplers is
+   pragma Preelaborate;
+
+   use GL.Objects.Samplers;
+   use GL.Types;
+   use all type GL.Types.Compare_Function;
+   use all type GL.Types.Colors.Border_Color;
+
+   type Wrapping_Mode_3D is array (Index_3D) of Wrapping_Mode;
+
+   type Sampler_State is record
+      Minifying_Filter   : Minifying_Function  := Nearest_Mipmap_Linear;
+      Magnifying_Filter  : Magnifying_Function := Linear;
+
+      Minimum_LoD        : Float_64 := -1000.0;
+      Maximum_LoD        : Float_64 :=  1000.0;
+      LoD_Bias           : Float_64 :=     0.0;
+      Max_Anisotropy     : Float_64 :=     1.0;
+
+      Seamless_Filtering : Boolean := False;
+
+      Wrapping : Wrapping_Mode_3D := (others => Repeat);
+
+      Border_Color             : Colors.Border_Color := Transparent_Black;
+      Compare_X_To_Texture     : Boolean             := False;
+      Current_Compare_Function : Compare_Function    := LEqual;
+   end record
+     with Dynamic_Predicate => Sampler_State.Max_Anisotropy >= 1.0;
+
+end Orka.Rendering.Samplers;
