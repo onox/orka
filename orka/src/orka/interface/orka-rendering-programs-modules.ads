660e2bd512beb97908597b64e07159c25694e54d
onox <denkpadje@gmail.com>
2023-10-21 15:55:35 +0200

    orka: Use 'private with' to import some packages

--- orka/src/orka/interface/orka-rendering-programs-modules.ads
+++ orka/src/orka/interface/orka-rendering-programs-modules.ads
@@ -14,9 +14,9 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 
-with Ada.Containers.Indefinite_Holders;
+private with Ada.Containers.Indefinite_Holders;
 
-with GL.Objects.Shaders;
+private with GL.Objects.Shaders;
 
 with Orka.Resources.Locations;
 
